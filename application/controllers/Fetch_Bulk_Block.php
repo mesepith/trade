3ba0c3b511785e8e3f0d54f92c9cbef30d2dd9eb
mesepith <zahiralam29@gmail.com>
2023-12-11 19:20:27 +0000

    bulk block deal

--- application/controllers/Fetch_Bulk_Block.php
+++ application/controllers/Fetch_Bulk_Block.php
@@ -3,8 +3,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 //include_once (dirname(__FILE__) . "/System_Notification_Controller.php");
-//include_once (dirname(__FILE__) . "/Nse_Contr.php");
+include_once (dirname(__FILE__) . "/Nse_Contr.php");
 include_once (dirname(__FILE__) . "/Send_Api_Contr.php");
+include_once (dirname(__FILE__) . "/Python_Controller.php");
 
 class Fetch_Bulk_Block extends MX_Controller {
     
@@ -17,7 +18,7 @@ class Fetch_Bulk_Block extends MX_Controller {
         $this->load->model('BulkBlock_model');
         
         $bulk_input_arr = array(
-            'nse'=>'https://www1.nseindia.com/content/equities/bulk.csv',
+            'nse'=>'https://www.nseindia.com/api/historical/bulk-deals?from=04-12-2023&to=11-12-2023&csv=true%22',
             'bse'=>'https://www.bseindia.com/markets/downloads/Bulk_'.date('dMY').'.csv',
         );
         
@@ -28,7 +29,7 @@ class Fetch_Bulk_Block extends MX_Controller {
         }
         
         $block_input_arr = array(
-            'nse'=>'https://www1.nseindia.com/content/equities/block.csv',
+            'nse'=>'https://www.nseindia.com/api/historical/block-deals?from=04-12-2023&to=11-12-2023&csv=true%22',
             'bse'=>'https://www.bseindia.com/markets/downloads/Block_'.date('dMY').'.csv',
         );
         
@@ -74,8 +75,8 @@ class Fetch_Bulk_Block extends MX_Controller {
             
             $csv_inp_data_arr= str_getcsv($bulk_block_csv_row_value);                         
             
-//            echo '<pre>';
-//            print_r($csv_inp_data_arr); exit;
+           echo '<pre>';
+           print_r($csv_inp_data_arr); exit;
             
             if( empty($csv_inp_data_arr) || empty($csv_inp_data_arr[0]) || empty($csv_inp_data_arr[1]) ){
 
@@ -143,6 +144,86 @@ class Fetch_Bulk_Block extends MX_Controller {
             $Send_Api_Contr->inserBulkBlockDeal($bulk_block_arr);
                 
         }
+    }
+
+    public function crawlBulkBlock($url, $referer, $bulk_or_block, $exchange ){
+
+        $Send_Api_Contr = new Send_Api_Contr();
+        $this->load->model('BulkBlock_model');
+
+        $is_inserted = $Send_Api_Contr->checkTodaysBulkBlockInserted($exchange, $bulk_or_block);
+
+        echo $bulk_or_block . '  is_inserted ' . $is_inserted; exit;
+        
+        if( $is_inserted > 0 ){ echo $bulk_or_block . ' of ' . $exchange . ' is already inserted '; return; }
+
+        $Python_contr = new Python_Controller();
+        $Nse_Contr = new Nse_Contr();
+
+        $Python_contr->executeCookieScript();
+
+        $data_return_arr = $Nse_Contr->curlNse($url, $referer);
+
+        
+
+
+        if( !empty($data_return_arr) && !empty($data_return_arr['data']) && !empty($data_return_arr['data']) ){
+
+            $bulk_block_arr = array();
+
+            foreach($data_return_arr['data'] AS $key=>$deal_value ){
+
+                $company_symbol = $deal_value['BD_SYMBOL'];
+
+                $company_id = $Send_Api_Contr->getCompanyIdAndIndexIdBySymbol( $company_symbol, $company_model_loaded=false );
+
+                // echo ' $company_id : ' . $company_id . '<br/>';
+
+                $market_date = date('Y-m-d', strtotime(trim($deal_value['BD_DT_DATE'])));
+
+                $bulk_block_arr['market_date'] = $market_date;
+                $bulk_block_arr['company_symbol'] = $company_symbol;
+                $bulk_block_arr['company_id'] = $company_id;
+                $bulk_block_arr['client_name'] = trim($deal_value['BD_CLIENT_NAME']);
+                $bulk_block_arr['buy_or_sale'] = strtolower(trim($deal_value['BD_BUY_SELL']));
+                $bulk_block_arr['quantity_traded'] = trim($deal_value['BD_QTY_TRD']);
+                $bulk_block_arr['trade_price'] = trim($deal_value['BD_TP_WATP']);
+
+                $bulk_block_arr['bulk_or_block'] = $bulk_or_block;
+                $bulk_block_arr['exchange'] = $exchange;
+
+                $bulk_block_arr["created_at"] = date("Y-m-d H:i:s");
+                $bulk_block_arr["updated_at"] = date("Y-m-d H:i:s");
+
+                $Send_Api_Contr->inserBulkBlockDeal($bulk_block_arr);
+
+                echo '<pre>';
+                print_r($bulk_block_arr);
+            }
+
+        }
+    }
+
+    function fetchNseBlockDeal(){
+
+        $currentDate = date('d-m-Y');
+        $dateOneWeekBefore = date('d-m-Y', strtotime('-1 week', strtotime($currentDate)));
+
+        $url = 'https://www.nseindia.com/api/historical/block-deals?from='.$dateOneWeekBefore.'&to=' . $currentDate;
+        $referer = 'https://www.nseindia.com/report-detail/display-bulk-and-block-deals';
+
+        $this->crawlBulkBlock($url, $referer, 'block', 'nse');
+    }
+
+    function fetchNseBulkDeal(){
+
+        $currentDate = date('d-m-Y');
+        $dateOneWeekBefore = date('d-m-Y', strtotime('-1 week', strtotime($currentDate)));
+        
+        $url = 'https://www.nseindia.com/api/historical/bulk-deals?from='.$dateOneWeekBefore.'&to=' . $currentDate;
+        $referer = 'https://www.nseindia.com/report-detail/display-bulk-and-block-deals';
+
+        $this->crawlBulkBlock($url, $referer, 'bulk', 'nse');
     }
     
 }
