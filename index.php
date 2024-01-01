f3ceccc1dc48a2eeeb615b78377c3f917f8145e5
mesepith <zahiralam29@gmail.com>
2024-01-01 14:02:39 +0000

    testing

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ setlocale(LC_MONETARY, 'en_IN');
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'testing');
 
 /*
  *---------------------------------------------------------------
