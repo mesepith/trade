c3872d2d2723f85ee57e95cf374e7f72412b8987
mesepith <zahiralam29@gmail.com>
2024-07-22 15:52:24 +0530

    eq percent min change

--- application/views/fii-dii/fii-sector.php
+++ application/views/fii-dii/fii-sector.php
@@ -316,7 +316,7 @@ $this->load->helper('function_helper');
                         <b><?php echo number_format($sum_equity); ?></b>
                         <?php 
 
-                        if( !empty($sector) &&  count($fii_sector_data)>0 ){
+                        if( !empty($sector) &&  count($fii_sector_data)>1 ){
 
                             $equity_percnt = percentOfTwoNumber( $fii_sector_data[$fii_sector_data_key]->equity, $fii_sector_data[0]->equity );
                         
