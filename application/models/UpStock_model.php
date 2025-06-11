d52fa60477eca0e2f1ff61081a293f07b17c089b
mesepith <zahiralam29@gmail.com>
2025-06-11 14:54:07 +0530

    min

--- application/models/UpStock_model.php
+++ application/models/UpStock_model.php
@@ -4,6 +4,10 @@
 @date: 14-May-2025
 */
 defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+@author: Zahir Alam
+@date: 14-May-2025
+*/
 class UpStock_model extends CI_Model {
     
     public function __construct() {
